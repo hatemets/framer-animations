d5bac8ed7d6da3f287d2fccf18a4589d0d4d8a10
hatemets <mistermark009@gmail.com>
2020-05-12 11:18:44 +0300

    Text animation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Frame, addPropertyControls, ControlType } from 'framer';
+import { Jump } from './Test';
 
-function App() {
+const App = () => {
+  const str = Array.from("Hello world");
+
+  const containerVariants = {
+     before: {},
+     after: { transition: { staggerChildren: 0.05  } },
+  };   
+
+  const letterVariants = {
+    before: {
+      opacity: 0,
+      y: 20,
+      transition: {
+        type: "string",
+        damping: 16,
+        stiffness: 200,
+      },
+    },
+    after: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: "string",
+        damping: 16,
+        stiffness: 200,
+      },
+    },
+  };
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <Jump/>  
+      <Frame
+      size={"100%"}
+      background={ "rgba(111, 111, 111, 0.5)" }>
+        <Frame
+        center={"y"}
+        height={26}
+        width={"100%"}
+        background={""}
+        style={{
+            fontFamily: "Montserrat, Work Sans, sans-serif",
+            fontWeight: "bold",
+            letterSpacing: "-0.04em",
+            fontSize: 26,
+            color: "#000",
+            display: "flex",
+            justifyContent: "center",
+        }}
+        variants={containerVariants}
+        initial={"before"}
+        animate={"after"}>
+      
+          {str.map((letter, i) => (
+            <Frame key={i}
+              width={"auto"}
+              height={26}
+              background={""}
+              variants={letterVariants}
+              style={{ position: "relative" }}>
+                { letter === " " ? "\u00A0" : letter }
+            </Frame>
+          ))}
+      </Frame>
+    </Frame>
     </div>
   );
 }
